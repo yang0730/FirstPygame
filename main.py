4289cd84f287e96121f85939e3a1aec891baf82a
yanglin <>
2022-06-27 09:15:36 +0000

    print block

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame
 pygame.init()
+def block(Coord):
+  pygame.draw.rect(window, (255,255,255), (Coord[0],Coord[1],10,10), border_radius=1)
+  
 height,width = 600,800
 BGC = 0,0,0
 window = pygame.display.set_mode((width,height))
@@ -7,3 +10,5 @@ pygame.display.set_caption("Deez Nut")
 window.fill(BGC)
 
 pygame.display.update()
+
+
