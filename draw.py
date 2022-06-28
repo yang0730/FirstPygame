f0c4160dbedf11c585525e153716cb6347cf8be2
yanglin <31237340+yang0730@users.noreply.github.com>
2022-06-28 06:45:44 +0000

    1

--- draw.py
+++ draw.py
@@ -1,6 +1,10 @@
 import pygame
 def block(wnd,x,y):
-  pygame.draw.rect(wnd, (255,255,255), (x,y,10,10), border_radius=0)
+  pygame.draw.rect(wnd, (255,255,255), (x*10,y*10,10,10), border_radius=0)
+  pygame.display.flip()
 
 def unblock(wnd,x,y):
-  pygame.draw.rect(wnd, (0,0,0), (x,y,10,10), border_radius=0)
+  pygame.draw.rect(wnd, (0,0,0), (x*10,y*10,10,10), border_radius=0)
+  pygame.draw.rect(wnd, (125,125,125), (x*10,y*10,10,1), border_radius=0)
+  pygame.draw.rect(wnd, (125,125,125), (x*10,y*10,1,10), border_radius=0)
+  pygame.display.flip()
--- game.py
+++ game.py
@@ -1,36 +1,76 @@
-import pygame,draw
-paused = False
+import pygame,draw,time
+paused = True
 Grid=[]
+PreGrid=[]
+
 def InitGrid():
-  for i in range(0,599):
+  for i in range(20):
     Grid.append([])
-    for j in range(0,799):
+    PreGrid.append([])
+    for j in range(15):
       Grid[i].append(0)
+      PreGrid[i].append(0)
+  print(len(Grid),len(Grid[1]))
 
 def check(x,y):
-  testX = [x-1,x,x+1]
-  testY = [y-1,y,y+1]
+  testX = [(x-1),x,(x+1)]
+  testY = [(y-1),y,(y+1)]
   result = 0
   for i in testX:
     for j in testY:
       if not(i==x and j==y):
-        if Grid[i][i] ==1:
+        if i > 20 or j >20 or i<0 or j<0:
+          pass
+        elif Grid[i][j] ==1:
           result += 1
-  if (result==2 or result ==3) and Grid[x][y] == 1 :
-    return True
+  if Grid[x][y] == 1 :
+    if (result==2 or result ==3):
+      pass
+      return True
+    else:
+      Grid[x][y]=0
+      return False
   elif Grid[x][y] ==0 and result == 3:
+    Grid[x][y] = 1
     return True
-  return False
+  else:
+    Grid[x][y]=0
+    return False
   
 def game(wnd):
+  count = 0
   if not paused:
-    for i in Grid:
-      for j in Grid[i]:
-        if Grid[i][j] != 0 and check(i,j):
-          draw.block(wnd,i,j)
+    for i in range(0,len(Grid)-1):
+      for j in range(0,len(Grid[i])-1):
+        if check(i,j):
+          if Grid[i][j] != PreGrid[i][j]:
+            count += 1
+            draw.block(wnd,i,j)
+        else:
+          if Grid[i][j] != PreGrid[i][j]:
+            draw.unblock(wnd,i,j)
+    updatePreGrid()
     pygame.display.update()
   if paused:
+    getclickedpos(wnd)
+  updateblocks(wnd)
+  time.sleep(0.5)
 
-    pygame.display.update()
 
+def updatePreGrid():
+  for i in range(len(Grid)):
+    for j in range(len(Grid[i])):
+      PreGrid[i][j] = Grid[i][j]
+    
+      
+def getclickedpos(wnd):
+  pass
 
+def updateblocks(wnd):
+  for i in range(0,len(Grid)-1):
+    for j in range(0,len(Grid[i])-1):
+      if Grid[i][j] ==1:
+        draw.block(wnd,i,j)
+      else:
+        draw.unblock(wnd,i,j)
+  pygame.display.update()
